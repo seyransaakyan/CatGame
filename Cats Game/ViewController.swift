b1836c35b4d14d0050e7237f0e51e70c48ba344b
Say <hajimesay@gmail.com>
2022-04-22 15:51:45 +0300

    added

---
+++ Cats Game/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Cats Game
+//
+//  Created by Seyran Saakyan on 22.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let button = UIButton(frame: CGRect(x: 160, y: 380, width: 100, height: 100))
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        button.layer.cornerRadius = button.frame.size.width/2
+        button.backgroundColor = .red
+        button.setTitle("", for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+
+        self.view.addSubview(button)
+    }
+    
+    @objc func buttonAction() {
+        button.frame.origin.x = CGFloat(randomCoordinates().0)
+        button.frame.origin.y = CGFloat(randomCoordinates().1)
+    }
+    
+    func randomCoordinates() -> (Int, Int){
+        let randomX = Int.random(in: 0..<314)
+        let randomY = Int.random(in: 0..<796)
+        return (randomX, randomY)
+    }
+}
+
